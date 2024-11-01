b024e3b819209edce5d04ee5f9ca4d99492ffda7
KamilFrant <kamilfrantmu@wp.pl>
2024-11-01 01:40:26 +0100

    init

---
+++ src/main/java/pl/kfrant/ceramicart/CeramicartApplication.java
@@ -0,0 +1,13 @@
+package pl.kfrant.ceramicart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeramicartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CeramicartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kfrant/ceramicart/CeramicartApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kfrant.ceramicart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeramicartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
